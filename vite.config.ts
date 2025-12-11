27d63ee20022859e39173199940bb72030caf109
1mpulse666 <artemerema2020@gmail.com>
2025-12-11 19:15:48 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -16,3 +16,12 @@ export default defineConfig(({ mode }) => ({
     },
   },
 }));
+
+  import { defineConfig } from 'vite'
+  import react from '@vitejs/plugin-react'
+  import tsconfigPaths from 'vite-tsconfig-paths'
+
+  export default defineConfig({
+  base: '/the-digital-canvas/',
+  plugins: [react(), tsconfigPaths()],
+})
